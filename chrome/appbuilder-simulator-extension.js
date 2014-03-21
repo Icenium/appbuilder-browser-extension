e2b1f837352714396101c96ad3658cdff995f35d
Jordan Ilchev <jordan@ilchev.com>
2014-03-21 17:31:36 +0200

    Limit to allow request specifically from icenium.com and telerik.com

--- chrome/appbuilder-simulator-extension.js
+++ chrome/appbuilder-simulator-extension.js
@@ -1,29 +1,61 @@
 // Copyright 2014 Telerik. All Rights Reserved.
 
 (function module() {
+	var consts,
+		requestQueue = {},
+		isAllowedOrigin = /(\.icenium\.com|\.telerik\.com)/;
 
-	//TODO alter only requests from app.icenium.com
+	consts = {
+		headers: {
+			ALLOW_ORIGIN: "Access-Control-Allow-Origin",
+			ORIGIN: "Origin"
+		}
+	};
 
-	function onHeadersReceivedHandler(details) {
-		var headers = details.responseHeaders,
-			hOrigin;
+	function getHeaderByKey(headers, key) {
+		var result;
 
 		for (var i = 0, l = headers.length; i < l; i++) {
-			if (headers[i].name === "Access-Control-Allow-Origin") {
-				hOrigin = headers[i];
+			if (headers[i].name === key) {
+				result = headers[i];
 				break;
 			}
 		}
+		return result;
+	}
 
-		if (!hOrigin) {
-			//TODO
-			hOrigin = { name: "Access-Control-Allow-Origin", value: "http://app.icenium.com" }
-			headers.push(hOrigin);
+	function onHeadersReceivedHandler(details) {
+		var request,
+			hAllowOrigin,
+			requestId = details.requestId,
+			headers = details.responseHeaders || [];
+
+		if (!requestQueue[requestId]) {
+			return;
 		}
 
+		hAllowOrigin = getHeaderByKey(headers, consts.headers.ALLOW_ORIGIN);
+		request = requestQueue[requestId];
+		if (!hAllowOrigin && request) {
+			hAllowOrigin = { name: consts.headers.ALLOW_ORIGIN, value: request.origin }
+			headers.push(hAllowOrigin);
+			delete requestQueue[requestId];
+		}
+		
 		return { responseHeaders: headers };
 	}
 
+	function onSendHeadersHandler(details) {
+		var requestId = details.requestId,
+			headers = details.requestHeaders || [],
+			hOrigin = getHeaderByKey(headers, consts.headers.ORIGIN);
+
+			if (hOrigin && isAllowedOrigin.test(hOrigin.value)) {
+				requestQueue[requestId] = { origin: hOrigin.value };
+			}
+
+		return { requestHeaders: headers };
+	}
 
 	// attach to events
 	chrome.webRequest.onHeadersReceived.addListener(
@@ -32,7 +64,16 @@
 			urls: ["<all_urls>"],
 			types: ["xmlhttprequest"]
 		},
-		["blocking", "responseHeaders"]
+		["blocking","responseHeaders"]
+	);
+
+	chrome.webRequest.onSendHeaders.addListener(
+		onSendHeadersHandler,
+		{
+			urls: ["<all_urls>"],
+			types: ["xmlhttprequest"]
+		},
+		["requestHeaders"]
 	);
 
 })(chrome);
